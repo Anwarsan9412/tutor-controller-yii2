65e90c4ef78a70759aeb8667cf0cf97a606b7bdb
Anwar Sanusi 12 <anwarsanusi9412@gmail.com>
2020-03-07 05:59:43 +0700

    publisch file index

---
+++ frontend/config/main-local.php
@@ -0,0 +1,25 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'Qqi6Jq8xVoF8thQ_4Rvqt7vo4PMkJh4S',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
---
+++ frontend/views/dashboard/index.php
@@ -0,0 +1,7 @@
+<?php
+echo 'Judul :' . $judul;
+echo '<br/> Profit :' . $index_profit;
+echo '</br> Produk Samsung :' . $produk['Samsung'];
+echo '</br> Produk Sony :' . $produk['Sony'];
+echo '</br> Produk Sharp :' . $produk['Sharp'];
+echo '</br> Produk LG :' . $produk['LG'];
